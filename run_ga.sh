b7c5b8f95982efa47a625eba1322fa52934fc57d
ngminhhieu <minhhieun4@gmail.com>
2020-11-03 14:38:48 +0700

    fix

--- run_ga.sh
+++ run_ga.sh
@@ -12,18 +12,18 @@ rm -rf "PM2_5-Prediction/log/*"
 cd "PM2_5-Prediction"
 tmux new-session -d -s real
 
-POP_WINDOW=( "pop_40" "pop_50" "pop_60" "pop_70" )
-index=(0 1 2 3)
-population=(40 50 60 70)
-for i in "${index[@]}"
-do
-    tmux send-keys -t real "tmux new-window -n ${POP_WINDOW[$i]} " ENTER
-done
+# POP_WINDOW=( "pop_40" "pop_50" "pop_60" "pop_70" )
+# index=(0 1 2 3)
+# population=(40 50 60 70)
+# for i in "${index[@]}"
+# do
+#     tmux send-keys -t real "tmux new-window -n ${POP_WINDOW[$i]} " ENTER
+# done
 
-for i in "${index[@]}"
-do
-    tmux send-keys -t real "tmux send-keys -t ${POP_WINDOW[$i]} 'python3 main.py --pc=0.8 --pm=0.2 --population=${population[$i]}' ENTER" ENTER
-done
+# for i in "${index[@]}"
+# do
+#     tmux send-keys -t real "tmux send-keys -t ${POP_WINDOW[$i]} 'python3 main.py --pc=0.8 --pm=0.2 --population=${population[$i]}' ENTER" ENTER
+# done
 
 PC_WINDOW=("pc_2" "pc_3" "pc_4" "pc_5" "pc_6" "pc_7" "pc_8")
 index=(0 1 2 3 4 5 6)
