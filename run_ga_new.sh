4c19ea8cbf11d32a25c17313ffa9c92a1c9d5ed4
Hieu Nguyen <minhhieun4@gmail.com>
2020-11-26 10:08:50 +0700

    fix file sh

---
+++ run_ga_new.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+if [ ! -d "PM2_5-Prediction" ]
+then
+git clone https://github.com/ngminhhieu/PM2_5-Prediction
+while [ ! -d "PM2_5-Prediction" ]
+do
+  sleep 2 # or less like 0.2
+done
+fi
+rm -rf "PM2_5-Prediction/log/*"
+cd "PM2_5-Prediction"
+git checkout ga
+tmux new-session -d -s real
+
+FIXED_SHUFFLE_WINDOW=( "fixed_false-backtest_0" "fixed_false-backtest_10" "fixed_false-backtest_20" "fixed_false-backtest_40" "fixed_false-backtest_60" "fixed_false-backtest_80" "fixed_false-backtest_100")
+index=(0 1 2 3 4 5 6)
+percentage_back_test=(0 10 20 40 60 80 100)
+for i in "${index[@]}"
+do
+    tmux send-keys -t real "tmux new-window -n ${FIXED_SHUFFLE_WINDOW[$i]} " ENTER
+done
+sleep 1
+for i in "${index[@]}"
+do
+    tmux send-keys -t real "tmux send-keys -t ${FIXED_SHUFFLE_WINDOW[$i]} 'python3 main.py --percentage_back_test=${percentage_back_test[$i]} --fixed=false' ENTER" ENTER
+done
+
+FIXED_WINDOW=( "fixed_false-shuffle_false-backtest_0" "fixed_false-shuffle_false-backtest_10" "fixed_false-shuffle_false-backtest_20" "fixed_false-shuffle_false-backtest_40" "fixed_false-shuffle_false-backtest_60" "fixed_false-shuffle_false-backtest_80" "fixed_false-shuffle_false-backtest_100")
+percentage_back_test=(0 10 20 40 60 80 100)
+for i in "${index[@]}"
+do
+    tmux send-keys -t real "tmux new-window -n ${FIXED_WINDOW[$i]} " ENTER
+done
+sleep 1
+for i in "${index[@]}"
+do
+    tmux send-keys -t real "tmux send-keys -t ${FIXED_WINDOW[$i]} 'python3 main.py --percentage_back_test=${percentage_back_test[$i]} --fixed=false --shuffle_gen=false' ENTER" ENTER
+done
+
+# End of outer loop.
+exit 0
