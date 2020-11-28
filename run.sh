b612921132aa42b92408c351efc3b7804972bc6d
Hieu Nguyen <minhhieun4@gmail.com>
2020-11-28 18:45:33 +0700

    fix

--- run.sh
+++ run.sh
@@ -13,7 +13,7 @@ cd "PM2_5-Prediction"
 tmux new-session -d -s real
 
 WINDOW=( "ffb0" "fffb10" "fffb20" "fffb40" "fffb60" "fffb80" "fffb100")
-WINDOW=( "ffshb0" "ffshb10" "ffshb20" "ffshb40" "ffshb60" "ffshb80" "ffshb100")
+FIXED_WINDOW=( "ffshb0" "ffshb10" "ffshb20" "ffshb40" "ffshb60" "ffshb80" "ffshb100")
 index=(0 1 2 3 4 5 6)
 percentage_back_test=(0 10 20 40 60 80 100)
 for i in "${index[@]}"
@@ -24,7 +24,7 @@ for i in "${index[@]}"
 do
     tmux send-keys -t real "tmux new-window -n ${FIXED_WINDOW[$i]} " ENTER
 done
-
+sleep 0.5
 for i in "${index[@]}"
 do
     tmux send-keys -t real "tmux send-keys -t ${WINDOW[$i]} 'python main.py --percentage_back_test=${percentage_back_test[$i]} --fixed=false --tmp=$i' ENTER" ENTER
