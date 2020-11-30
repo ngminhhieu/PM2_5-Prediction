b2c07a26fa42a181bcf90ef607da86369cf2de52
Hieu Nguyen <minhhieun4@gmail.com>
2020-11-30 11:49:06 +0700

    check if see GPU

--- main.py
+++ main.py
@@ -20,14 +20,14 @@ def checkGPU():
     print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
     gpus = tf.config.experimental.list_physical_devices('GPU')
     if gpus:
-    # Restrict TensorFlow to only use the first GPU
-    try:
-        tf.config.experimental.set_visible_devices(gpus[0], 'GPU')
-        logical_gpus = tf.config.experimental.list_logical_devices('GPU')
-        print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPU")
-    except RuntimeError as e:
-        # Visible devices must be set before GPUs have been initialized
-        print(e)
+        # Restrict TensorFlow to only use the first GPU
+        try:
+            tf.config.experimental.set_visible_devices(gpus[0], 'GPU')
+            logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+            print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPU")
+        except RuntimeError as e:
+            # Visible devices must be set before GPUs have been initialized
+            print(e)
 
 def seed():
     # The below is necessary for starting Numpy generated random numbers
