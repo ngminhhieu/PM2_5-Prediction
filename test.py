26d0591e600c37b9b028023626f9ea4dfcd908a9
Hieu Nguyen <minhhieun4@gmail.com>
2021-03-04 14:48:49 +0700

    prepare to train hanoi

--- test.py
+++ test.py
@@ -1,52 +1,6 @@
-# from tensorflow.keras.models import Model
-# from tensorflow.keras.layers import Input
-# from tensorflow.keras.layers import LSTM
-# from numpy import array
-# # define model
-# inputs1 = Input(shape=(3, 1))
-# lstm1, state_h, state_c = LSTM(1, return_sequences=True, return_state=True)(inputs1)
-# model = Model(inputs=inputs1, outputs=[lstm1, state_h, state_c])
-# # define input data
-# data = array([0.4, 0.2, 0.3]).reshape((1,3,1))
-# # make and show prediction
-# print(model.predict(data))
-import random
 import pandas as pd
 import numpy as np
-# df = pd.read_csv('data/csv/hanoi_data_full.csv', usecols=['WIND_SPEED', 'TEMP', 'RADIATION', 'PM10', 'PM2.5'])
 
-# df = df.groupby(np.arange(len(df.index))//24).mean()
-# df.to_csv('data/csv/hanoi_data_full_day.csv')
-# np.savez('data/npz/hanoi/hanoi_data_full_day.npz', monitoring_data = df)
-
-
-# dataset =  pd.read_csv('data/csv/ga/dataset_train.csv')
-# pivot = int(10 * len(dataset) / 100)
-# for i in range(100000):
-#     random_start_point = random.randint(0, len(dataset) - pivot)
-#     tmp_dataset = dataset.iloc[random_start_point:random_start_point + pivot + 1000]
-#     print(i)
-#     if tmp_dataset.isnull().values.any():
-#         print(random_start_point)
-#         print(pivot)
-#         print(tmp_dataset)
-
-import datetime
-currentTime = datetime.datetime.now()
-print(str(currentTime))
-
-import psutil
-# gives a single float value
-print(psutil.cpu_percent())
-# gives an object with many fields
-print(psutil.virtual_memory())
-# you can convert that object to a dictionary 
-print(dict(psutil.virtual_memory()._asdict()))
-# you can have the percentage of used RAM
-print(psutil.virtual_memory().percent)
-# you can calculate percentage of available memory
-print(psutil.virtual_memory().available * 100 / psutil.virtual_memory().total)
-
-
-test_df = pd.read_csv('./data/csv/taiwan_data_mean.csv', usecols=['AMB_TEMP', 'RH', 'WIND_DIREC', 'WIND_SPEED','PM10', 'PM2.5'])
-np.savez('./data/npz/taiwan/taiwan.npz', monitoring_data = test_df)
+dataset = pd.read_csv("./data/csv/taiwan.csv", usecols=['AMB_TEMP', 'RH', 'WIND_DIREC', 'WIND_SPEED', 'PM10', 'PM2.5'])
+dataset = dataset.to_numpy()
+np.savez("./data/npz/taiwan/taiwan.npz", monitoring_data = dataset)
