8ed7af5e700ffdac8a6e142fe0cabb69a5e1c418
thanhhff <dataholicguy@gmail.com>
2021-11-08 16:12:41 +0700

    add log

--- manually_select_features.py
+++ manually_select_features.py
@@ -1,6 +1,11 @@
 from lib.preprocessing_data import generate_npz
 if __name__ == '__main__':
-    input_features = ["WIND_DIR", "PM1", "PM2.5"]
-    generate_npz(input_features, 'data/csv/hanoi.csv', 'data/npz/new_training_mechanism/hanoi.npz',
-             'config/hanoi/new_training_mechanism_1.yaml',
-             'log/new_training_mechanism/')
+    # input_features = ["WIND_DIR", "PM1", "PM2.5"]
+    input_features = ["MONTH", "YEAR", "WIND_SPEED", "RH", "PM1", "PM2.5"]
+    # input_features = ["YEAR", "HOUR", "WIND_SPEED", "TEMP", "BAROMETER", "RADIATION", "PM10", "PM1", "PM2.5"]
+    # input_features = ["RADIATION", "PM10", "PM1", "PM2.5"]
+    # input_features = ["DAY", "HOUR", "WIND_DIR", "TEMP", "PM2.5"]
+    for i in range(1, 7):
+        generate_npz(input_features, 'data/csv/hanoi.csv', 'data/npz/new_training_mechanism/hanoi2.npz',
+                'config/hanoi/new_training_mechanism_{}.yaml'.format(str(i)),
+                'log/new_training_mechanism/')
